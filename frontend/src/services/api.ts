d9e087a1947cf944c1b6c1e33fc4bf3669d4d2c7
Rutuja Khande <rutujakhande99@gmail.com>
2026-08-13 22:43:19 +0530

    initial

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { RecommendationResponse, SearchResponse, SongDetail } from '../types'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const baseURL = import.meta.env.VITE_API_URL ||  'https://melodymatch-c40m.onrender.com'
 
 export const api = axios.create({
   baseURL,
